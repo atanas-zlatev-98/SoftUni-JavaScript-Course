35e10e2803ff7e8429fbb35c78c0643efdbe7092
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-10-25 21:47:34 +0300

    Objects - Cats

---
+++ Fundamentals/Lectures/Objects and Classes/Cat.js
@@ -0,0 +1,32 @@
+function cats(catsAsStr){
+
+    class Cat {
+        name;
+        age;
+    
+        constructor(name,age){
+            this.name = name;
+            this.age = age;
+        }
+    
+        meow(){
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for(let catAsStr of catsAsStr){
+      let tokens = catAsStr.split(' ');
+
+      let name = tokens[0]
+      let age = Number(tokens[1]);
+
+      let cat = new Cat(name,age);
+
+      cat.meow();
+
+    }
+}
+
+
+
+cats(['Candy 1', 'Poppy 3', 'Nyx 2']);
