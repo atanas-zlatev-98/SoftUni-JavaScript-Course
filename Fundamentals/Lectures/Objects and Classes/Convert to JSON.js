2df6c1caa7a5685415e3e09e0ee8ef686f09fc0f
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-10-25 22:28:14 +0300

    Objects - Convert to JSON

---
+++ Fundamentals/Lectures/Objects and Classes/Convert to JSON.js
@@ -0,0 +1,16 @@
+function toJson(name,lastName,hairColor){
+
+    let person = {
+
+        name,
+        lastName,
+        hairColor
+        
+    }
+
+    console.log(JSON.stringify(person));
+
+
+}
+
+toJson('George', 'Jones','Brown')
