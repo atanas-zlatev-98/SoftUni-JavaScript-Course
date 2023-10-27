d76e6978ade353744011623c2d218de01e8bc66e
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-10-27 18:45:52 +0300

    Objects Exercise - Employees

---
+++ Fundamentals/Lab/Objects and Classes/Employees.js
@@ -0,0 +1,10 @@
+function employees(arr){
+
+    for(let name of arr){
+        let employee = { name, personalNum: name.length };
+        console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`)
+    }
+
+}
+
+employees(['Silas Butler','Adnaan Buckley','Juan Peterson','Brendan Villarreal']);
