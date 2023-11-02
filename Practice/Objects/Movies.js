188cad669a8bf884c1e672f6a5a56cb62aa01ca4
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-02 21:51:33 +0200

    Practice - Objects

---
+++ Practice/Objects/Movies.js
@@ -0,0 +1,59 @@
+function movies(arr) {
+    
+  let movies = [];
+
+  for (let movie of arr) {
+
+    if (movie.includes("addMovie")) {
+
+      let command = movie.split("addMovie ");
+      let movieName = command[1];
+
+      let moviez = {
+            name: movieName,
+      }
+      movies.push(moviez);
+    }else if(movie.includes('directedBy')){
+        let command = movie.split(' directedBy ');
+        let [movieName, directorName] = command;
+
+       let findMovie = movies.find(movie => movie.name == movieName);
+
+        if(findMovie){
+            findMovie.director = directorName;
+        }
+    }else if(movie.includes('onDate')){
+
+        let command = movie.split(' onDate ');
+        let [movieName, date] = command;
+
+        let findMovie = movies.find(movie => movie.name == movieName);
+
+        if(findMovie){
+            findMovie.date = date;
+        }
+        
+    }
+  }
+  for(let allMovies of movies) {
+    console.log(allMovies);
+  }
+}
+
+movies([
+  "addMovie Fast and Furious",
+
+  "addMovie Godfather",
+
+  "Inception directedBy Christopher Nolan",
+
+  "Godfather directedBy Francis Ford Coppola",
+
+  "Godfather onDate 29.07.2018",
+
+  "Fast and Furious onDate 30.07.2018",
+
+  "Batman onDate 01.08.2018",
+
+  "Fast and Furious directedBy Rob Cohen",
+]);
