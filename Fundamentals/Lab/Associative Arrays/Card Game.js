7ad6ceb778319ad831345006d0380cd38b5a2464
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-08 22:50:44 +0200

    Associative Arrays Exercise - Card Game

--- Fundamentals/Lab/Associative Arrays/Card Game.js
+++ Fundamentals/Lab/Associative Arrays/Card Game.js
@@ -3,7 +3,7 @@ function cardGame(arr) {
   let powerValues = {2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,J:11,Q:12,K:13,A:14};
     let typesValues = {S:4,H:3,D:2,C:1};
   for (let playerDeck of arr) {
-    let [playerName, cardsStr] = playerDeck.split(":");
+    let [playerName, cardsStr] = playerDeck.split(": ");
     let cards = cardsStr.split(', ');
 
     if(playerName in playersCards){
