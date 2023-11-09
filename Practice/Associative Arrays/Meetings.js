6561dc037e900cfcbab47c17c76ac05ed42b07df
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-09 19:02:03 +0200

    Practice - Associative Arrays

---
+++ Practice/Associative Arrays/Meetings.js
@@ -0,0 +1,28 @@
+function meetings(arr){
+
+    let meetings = {};
+
+    for(let meeting of arr){
+        let [weekday,person] = meeting.split(' ');
+
+        if(weekday in meetings){
+            console.log(`Conflict on ${weekday}!`);
+        }else {
+            meetings[weekday] = person;
+            console.log(`Scheduled for ${weekday}`);
+        }
+    }
+
+    for(let [day,name] of Object.entries(meetings)){
+        console.log(`${day} -> ${name}`);
+    }
+
+}
+
+meetings(['Monday Peter',
+
+'Wednesday Bill',
+
+'Monday Tim',
+
+'Friday Tim'])
