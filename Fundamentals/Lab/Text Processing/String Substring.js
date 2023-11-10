f3a385799451016b48223dfbd68462032668ef54
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-10 18:40:34 +0200

    Text Processing Exercise - String Substring

---
+++ Fundamentals/Lab/Text Processing/String Substring.js
@@ -0,0 +1,13 @@
+function strSubst(word,text){
+
+    text = text.toLowerCase().split(' ');
+    
+    if(text.includes(word)){
+        console.log(word);
+    }else {
+        console.log(`${word} no found.`);
+    }
+
+}
+
+strSubst('javascript','JavaScript is the best programming language');
