25302437254cb8b2f75e0a0a5242ce2b21e8d565
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-10 19:10:43 +0200

    Text Processing Exercise - Reveal Words

---
+++ Fundamentals/Lab/Text Processing/Reveal Words.js
@@ -0,0 +1,18 @@
+function revealWords(wordStr, text) {
+  let words = wordStr.split(" ");
+
+  for (let word of words) {
+    let count = word.length;
+    let starTemplate = "*".repeat(count);
+
+    text = text.replace(starTemplate, word);
+  }
+
+  console.log(text);
+}
+
+revealWords(
+  "great",
+
+  "softuni is ***** place for learning new programming languages"
+);
