25ec7dbbe750614b550ca05c05bf5758186b5bfb
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2023-11-26 16:35:36 +0200

    Final Exam Prep

---
+++ Fundamentals/Final Exam Preparation/Regex/Destination Mapper.js
@@ -0,0 +1,27 @@
+function destinationMapper(str){
+
+    let message = str;
+
+    let pattern = /([=|\/])(?<destination>[A-Z][A-Za-z]{3,})\1/g;
+
+    let match = pattern.exec(message);
+    let travelPoints = 0;
+    let destinations = [];
+
+    while(match !== null){
+
+        let {destination} = match.groups;
+
+        destinations.push(destination);
+        travelPoints += destination.length;
+        
+        match = pattern.exec(message);
+    }
+
+    console.log(`Destinations: ${destinations.join(', ')}`);
+    console.log(`Travel Points: ${travelPoints}`);
+
+}
+
+destinationMapper("=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=");
+destinationMapper("ThisIs some InvalidInput");
