eee1f0bb1deca9e0c75c2f329d8dc5d32c4bb8ce
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-01-12 22:40:22 +0200

    JS Advanced - Arrays and Nested Arrays

--- JS Advanced/Lab/Arrays and Nested Arrays/Last K Numbers Sequence.js
+++ JS Advanced/Lab/Arrays and Nested Arrays/Last K Numbers Sequence.js
@@ -1,13 +1,13 @@
 function lastKNumbersSequence(n, k) {
 
     let result = [1];
-    
+
     for (let i = 1; i < n; i++) {
       let start = Math.max(0, i - k);
       let end = i + k;
   
-      let currentElement = result.slice(start, end).reduce((acc, curr) => acc + curr);
-      result.push(currentElement);
+      let current = result.slice(start, end).reduce((acc, curr) => acc + curr);
+      result.push(current);
     }
   
     return result;
