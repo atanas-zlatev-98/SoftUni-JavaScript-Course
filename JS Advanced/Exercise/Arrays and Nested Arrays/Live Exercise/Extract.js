be2f362df5ec49a2b10aa3fc6bb7c9503f4bcc32
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-01-15 18:45:44 +0200

    JS Advanced Live Exercise - Arrays and Nested Arrays

--- JS Advanced/Exercise/Arrays and Nested Arrays/Live Exercise/Extract.js
+++ JS Advanced/Exercise/Arrays and Nested Arrays/Live Exercise/Extract.js
@@ -1,21 +1,37 @@
-function extract(arr){
+function extract(arr) {
+  let result = [];
+  let biggestOne = arr.shift();
 
-    let result = [];
-    let biggestOne = arr.shift();
+  result.push(biggestOne);
 
-    result.push(biggestOne);
+  arr.forEach((x) => {
+    if (biggestOne <= x) {
+      biggestOne = x;
+      result.push(biggestOne);
+    }
+  });
 
-    arr.forEach(x => {
-        if(biggestOne < x){
-            biggestOne = x;
-            result.push(biggestOne);
-        }
-    })
-
-    return result;
-    
+  return result;
 }
 
-extract([1, 3, 8, 4, 10, 12, 3, 2, 24]);
-extract([1, 2, 3, 4]);
-extract([20,3,2,15,6,1]);
+console.log(extract([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+console.log(extract([1, 2, 3, 4]));
+console.log(extract([20, 20, 3, 2, 15, 6, 1]));
+
+console.log("-------------");
+
+function extract2(arr) {
+  let biggestOne = arr[0];
+
+  return arr.filter((x) => {
+    if (x >= biggestOne) {
+      biggestOne = x;
+      return true;
+    }
+    return false;
+  });
+}
+
+console.log(extract2([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+console.log(extract2([1, 2, 3, 4]));
+console.log(extract2([20, 20, 3, 2, 15, 6, 1]));
