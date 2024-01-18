e877309e33ee8e403eaa5d902357d4420ccf865d
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-01-18 18:27:11 +0200

    JS Advanced Live Exercise - Objects and Composition

---
+++ JS Advanced/Exercise/Objects and Composition/Live Exercise/Construction Crew.js
@@ -0,0 +1,16 @@
+function constructionCrew(obj) {
+
+    if(obj.dizziness){
+        obj.levelOfHydrated += obj.weight * obj.experience * 0.1;
+        obj.dizziness = !obj.dizziness;
+    }
+
+    return obj
+
+}
+console.log(constructionCrew({
+  weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true,
+}));
