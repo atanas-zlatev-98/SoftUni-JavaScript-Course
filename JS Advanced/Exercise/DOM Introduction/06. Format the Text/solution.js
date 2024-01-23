f6b9b5cde7e7743c63d284a565fc7830fbf6a55f
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-01-23 18:02:08 +0200

    Folder Remane

--- JS Advanced/Exercise/DOM Introduction/06. Format the Text/solution.js
+++ JS Advanced/Exercise/DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+
+  let inputRef = document.getElementById("input");
+  let result = document.getElementById("output");
+
+  let inputRefValue = inputRef.value;
+
+  let splitText = inputRefValue.split(".").filter((x) => x.length > 0);
+
+  for (let i = 0; i < splitText.length; i += 3) {
+    let text = [];
+    for (let k = 0; k < 3; k++) {
+      if (splitText[i + k]) {
+        text.push(splitText[i + k]);
+      }
+    }
+    result.innerHTML += `<p>${text.join(". ") + "."}</p>`;
+  }
 }
