a2e22659b93e2bc10dad599dba768fe1b6b46cd1
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-01-27 22:52:53 +0200

    JS Advanced Exercise - DOM Manipulation and Events

--- JS Advanced/Lab/Advanced Functions/Cars.js
+++ JS Advanced/Lab/Advanced Functions/Cars.js
@@ -1,22 +1,42 @@
 function cars(arr) {
     let data = {};
-    let commands = {
-        create,
-        set,
-        print
+  
+    const createCar = {
+      create,
+      set,
+      print,
+    };
+  
+    function create(carName, inherits, carNameTwo) {
+      data[carName] = inherits ? Object.create(data[carNameTwo]) : {}
     }
-
-    function create(name, inherit,parent){
-        
+  
+    function set(carName, carColor, color) {
+      data[carName][carColor] = color;
     }
-
-}
-
-cars([
-  "create c1",
-  "create c2 inherit c1",
-  "set c1 color red",
-  "set c2 model new",
-  "print c1",
-  "print c2",
-]);
+  
+    function print(carName) {
+      const entry = [];
+  
+      for (const key in data[carName]) {
+        entry.push(`${key}:${data[carName][key]}`);
+      }
+  
+      console.log(entry.join(","));
+    }
+  
+    for (let car of arr) {
+      let [command, carName, inherit, carNameTwo] = car.split(" ");
+      createCar[command](carName, inherit, carNameTwo);
+    }
+  }
+  
+  cars([
+    "create c1",
+    "create c2 inherit c1",
+    "set c1 color red",
+    "set c2 model new",
+    "print c1",
+    "print c2",
+  ]);
+  
