489dcf8189be008bd5712aec1858c238f5835289
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2024-02-02 23:20:44 +0200

    Form Validation - Unit Testing

--- JS Advanced/Exercise/Unit Testing and Error Handling/07.Form-Validation/formValidation.js
+++ JS Advanced/Exercise/Unit Testing and Error Handling/07.Form-Validation/formValidation.js
@@ -1,19 +1,131 @@
 function validate() {
-    const submitBtn = document.getElementById('submit');
-    const usernameInputRef = document.getElementById('username');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-    const confirmPasswordInput = document.getElementById('confirm-password');
+  const submitBtn = document.querySelector("#submit");
 
-    let usernamePatterns = /[a-zA-Z0-9]+/
-    submitBtn.addEventListener('click', (e)=>{
+  const userNameRef = document.querySelector("#username");
 
-        e.preventDefault();
+  const emailRef = document.querySelector("#email");
 
-        if(usernameInputRef.value.length >= 3 && usernameInputRef.value.length <= 20){
-            usernameInputRef.style.borderColor = 'none';
+  const passwordRef = document.querySelector("#password");
+  const confirmPasswordRef = document.querySelector("#confirm-password");
+
+  const checkBoxRef = document.querySelector("#company");
+  const companyInfoRef = document.querySelector("#companyInfo");
+  const companyNumberRef = document.querySelector('#companyNumber');
+
+  const validRef = document.getElementById("valid");
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    gatherInfo();
+  });
+
+  checkBoxRef.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      companyInfoRef.style.display = "block";
+    } else {
+      companyInfoRef.style.display = "none";
+    }
+  });
+
+  function gatherInfo() {
+    const userNameValue = userNameRef.value;
+    const emailValue = emailRef.value;
+    const passwordValue = passwordRef.value;
+    const confirmPasswordValue = confirmPasswordRef.value;
+    const checkBoxValue = checkBoxRef.checked;
+    const companyNumberValue = companyNumberRef.value;
+
+    let isUserValid = checkUsername(userNameValue);
+    let isEmailValid = checkEmail(emailValue);
+    let isPasswordValid = checkPassword(passwordValue, confirmPasswordValue);
+    let isCompanyValid = checkCompanyNumber(companyNumberValue);
+
+    if (userNameValue) {
+      checkUsername(userNameValue);
+    }
+
+    if (emailValue) {
+      checkEmail(emailValue);
+    }
+
+    if (passwordValue) {
+      checkPassword(passwordValue, confirmPasswordValue);
+    }
+
+    if(companyNumberValue && checkBoxValue){
+        checkCompanyNumber(companyNumberValue);
+    }
+
+    if (!checkBoxValue) {
+        checkValidNoCompany(isUserValid,isEmailValid,isPasswordValid);
+      } else {
+        checkValidCompany(isUserValid,isEmailValid,isPasswordValid,isCompanyValid);
+      }
+  }
+
+  function checkUsername(username) {
+    let usernamePatterns = /^[A-Za-z0-9]{3,20}$/;
+
+    if (username.match(usernamePatterns)) {
+      userNameRef.style.border = "none";
+      return true;
+    } else {
+      userNameRef.style.borderColor = "red";
+      return false;
+    }
+  }
+
+  function checkEmail(email) {
+    let emailPattern = /^[^@.]+@[^@]*\.[^@]*$/;
+
+    if (email.match(emailPattern)) {
+      emailRef.style.border = "none";
+      return true;
+    } else {
+      emailRef.style.borderColor = "red";
+      return false;
+    }
+  }
+
+  function checkPassword(password, confirmPassword) {
+    let passwordPattern = /^[A-Za-z0-9_]{5,15}$/;
+
+    if (password.match(passwordPattern) && password === confirmPassword) {
+      passwordRef.style.border = "none";
+      confirmPasswordRef.style.border = "none";
+      return true;
+    } else {
+      passwordRef.style.borderColor = "red";
+      confirmPasswordRef.style.borderColor = "red";
+      return false;
+    }
+  }
+
+  function checkValidNoCompany(user,email,password) {
+    if (user && email && password) {
+      validRef.style.display = "block";
+    }else {
+        validRef.style.display = "none";
+    }
+  }
+
+  function checkValidCompany(user,email,password,company) {
+    
+    if (user && email && password && company) {
+      validRef.style.display = "block";
+    }else {
+        validRef.style.display = "none";
+    }
+  }
+
+  function checkCompanyNumber(companyNum){
+        if(companyNum >= 1000 && companyNum <= 9999){
+            companyNumberRef.style.border = "none";
+            return true
         }else {
-            usernameInputRef.style.borderColor = 'red';
+            companyNumberRef.style.borderColor = "red";
+            return false
         }
-    })
+  }
+
 }
